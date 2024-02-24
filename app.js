3753ca327640403ad30765cfc6a313140383a631
fedeholc <federicoholc@gmail.com>
2024-02-24 14:08:33 -0300

    Refactor CRUD operations in app.js

--- app.js
+++ app.js
@@ -44,23 +44,15 @@ app.use(bodyParser.json());
 app.get("/", async function (req, res) {
   try {
     let results = await tursoClient.execute("SELECT * FROM note");
-    console.log("res:", results);
     res.status(200).json(results.rows);
   } catch (err) {
-    console.log("eeeeeeer", err);
+    console.log("Get Error: ", err);
     res.status(400).send(err);
   }
-  /*  if (!err) {
-    res.status(200).json(results);
-  } else {
-    console.error(err); // results contains rows returned by server
-    res.status(400).send(err);
-  } */
 });
 
-// TODO: Poner todos los campos en la consulta SQL
-// TODO: pasar la consulta a una variable
-app.post("/", function (req, res) {
+//? post viejo en MySQL
+/* app.post("/", function (req, res) {
   let postQuery = `INSERT INTO ${TABLE_NAME} 
     (id, noteText, noteHTML, noteTitle, tags, category, deleted, archived, reminder, rating, created, modified) 
     VALUES (
@@ -85,11 +77,38 @@ app.post("/", function (req, res) {
       res.status(400).send(err);
     }
   });
+}); */
+
+app.post("/", async function (req, res) {
+  try {
+    let query = `INSERT INTO ${TABLE_NAME} 
+    (id, noteText, noteHTML, noteTitle, tags, category, deleted, archived, reminder, rating, created, modified) 
+    VALUES (
+      '${req.body.id}', 
+      '${escaparComillasSimples(req.body.noteText)}', 
+      '${escaparComillasSimples(req.body.noteHTML)}',
+      '${escaparComillasSimples(req.body.noteTitle)}',
+      '${req.body.tags}',
+      '${req.body.category}',
+      ${req.body.deleted},
+      ${req.body.archived},
+      '${req.body.reminder}',
+      ${req.body.rating},
+      '${req.body.created}',
+      '${req.body.modified}'
+    )`;
+
+    let results = await tursoClient.execute(query);
+
+    res.status(201).json(results.rowsAffected);
+  } catch (err) {
+    console.log("Post Error: ", err);
+    res.status(400).send(err);
+  }
 });
 
-// TODO: Poner todos los campos en la consulta SQL
-// TODO: pasar la consulta a una variable
-app.put("/", function (req, res) {
+//? put viejo en MySQL
+/* app.put("/", function (req, res) {
   connection.execute(
     `UPDATE ${TABLE_NAME} SET id = '${req.body.id}', 
     noteText = '${escaparComillasSimples(req.body.noteText)}', 
@@ -118,10 +137,35 @@ app.put("/", function (req, res) {
       }
     }
   );
+}); */
+
+app.put("/", async function (req, res) {
+  try {
+    let query = `UPDATE ${TABLE_NAME} SET id = '${req.body.id}', 
+    noteText = '${escaparComillasSimples(req.body.noteText)}', 
+    noteHTML = '${escaparComillasSimples(req.body.noteHTML)}', 
+    noteTitle = '${escaparComillasSimples(req.body.noteTitle)}',
+    tags = '${req.body.tags}',
+    category = '${req.body.category}',
+    deleted = ${req.body.deleted},
+    archived = ${req.body.archived},
+    reminder = '${req.body.reminder}',
+    rating = ${req.body.rating},
+    created = '${req.body.created}',
+    modified = '${req.body.modified}'
+    WHERE id = '${req.body.id}'`;
+
+    let results = await tursoClient.execute(query);
+
+    res.status(201).json(results.rowsAffected);
+  } catch (err) {
+    console.log("Put Error: ", err);
+    res.status(400).send(err);
+  }
 });
 
-// TODO: pasar la consulta a una variable
-app.delete("/del/:Id", function (req, res) {
+//? get viejo en MySQL
+/* app.delete("/del/:Id", function (req, res) {
   connection.execute(
     `DELETE FROM ${TABLE_NAME} WHERE id = ?`,
     [req.params.Id],
@@ -134,6 +178,20 @@ app.delete("/del/:Id", function (req, res) {
       }
     }
   );
+}); */
+
+app.delete("/del/:Id", async function (req, res) {
+  try {
+    let query = `DELETE FROM ${TABLE_NAME} WHERE id = '${req.params.Id}'`;
+    console.log("query:", query);
+    let results = await tursoClient.execute(query);
+
+    console.log("query:", query);
+    res.status(204).json(results.rowsAffected);
+  } catch (err) {
+    console.log("Delete Error: ", err);
+    res.status(400).send(err);
+  }
 });
 
 app.use(function (req, res) {
